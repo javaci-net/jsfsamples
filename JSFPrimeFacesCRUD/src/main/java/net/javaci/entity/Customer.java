ea4fdc0854da8d538b074562a46f3b6d0597aa6e
volkanistek <volkanistek@gmail.com>
2019-12-07 16:55:42 +0300

    init JSFPrimeFacesCRUD

---
+++ JSFPrimeFacesCRUD/src/main/java/net/javaci/entity/Customer.java
@@ -0,0 +1,53 @@
+package net.javaci.entity;
+
+import java.util.Date;
+
+public class Customer {
+
+	private String name;
+	private String surname;
+	private String phoneNumber;
+	private String email;
+	private Date birthDay;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDay() {
+		return birthDay;
+	}
+
+	public void setBirthDay(Date birthDay) {
+		this.birthDay = birthDay;
+	}
+
+}
